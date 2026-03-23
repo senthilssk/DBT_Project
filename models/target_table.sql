4e428e89bc43cad8d04761aec083f36f619fab63
senthilssk <senthil.sundaram@intellectdesign.com>
2026-03-23 04:04:41 +0000

    c

--- models/target_table.sql
+++
@@ -1,7 +0,0 @@
-{{ config(materialized='table') }}
-
--- Replace RAW_DB with your actual Snowflake Database name
--- Replace RAW_SCHEMA with your actual Snowflake Schema name
--- Replace ORIGINAL_100_TABLE with the table that HAS the records
-
-SELECT * FROM DBT_DB.DBT_SCHEMA.dbt_customer
